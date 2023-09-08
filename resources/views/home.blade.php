02b9ccc6d27a7d396ff49ab37d0bc9c40caab45c
MonicaIzzo <info@monicaizzo.net>
2023-09-08 06:25:26 +0200

    setup routes, layout, include

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.layout')
+
+{{-- Setto il titolo della pagina --}}
+@section('title', 'Home')
+
+{{-- Setto il main della pagina (che cambiera in base alla pagina) --}}
+@section('main_content')
+    ciao
+@endsection
---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ env('APP_NAME') }} | @yield('title')</title>
+
+
+    {{-- Pacchetti esterni --}}
+    @yield('cdns')
+
+    <!-- Style-->
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    {{-- Header --}}
+    @include('includes.header')
+
+    {{-- Contenuto del MAIN che varia ad ogni PAGINA --}}
+    <main>
+        @yield('main_content')
+    </main>
+
+    {{-- Script JS dedicati --}}
+    @yield('scripts')
+</body>
+
+</html>
